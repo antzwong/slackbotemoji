b29cb67122ed807dfa8da19c4e84c97ddadcd21e
Anthony <anthonywongfl99@gmail.com>
2019-10-26 18:47:51 -0400

    added chuck norris

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const bot =  new SlackBot({
   //INSERT TOKEN HERE BUT DO NOT UPLOAD TO GIT
-	token: '',
+	token: 'xoxb-809782627460-797647947938-0nZ2GSiiFOKj1He39O5DiKzC',
 	name: 'emojisrgr8'
 });
 
@@ -18,6 +18,20 @@ bot.on('start', () => {
 function handleMessage(message) {
   //call whatever ml sentiment voodoo api then have it return an emoji
 }
+
+function norrisJoke () {
+	axios.get('http://api.icndb.com/jokes/random').then(res => {
+    const joke = res.data.value.joke;
+
+    const params = {
+      icon_emoji: ':laughing:'
+    };
+
+    bot.postMessageToChannel('general', `Chuck Norris: ${joke}`, params);
+  });
+}
+
+
 
 // Error handling
 bot.on('error', err => console.log('error'));
